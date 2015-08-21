bb13c5c82a70f521090ab1ad8355175b6256608a
mayreach86 <mayreach@163.com>
2015-08-21 10:42:47 +0800

    初始化

---
+++ FirstNoGit/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  FirstNoGit
+//
+//  Created by 谢寅龙 on 15/8/21.
+//  Copyright (c) 2015年 谢寅龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ FirstNoGit/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  FirstNoGit
+//
+//  Created by 谢寅龙 on 15/8/21.
+//  Copyright (c) 2015年 谢寅龙. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
